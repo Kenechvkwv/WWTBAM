ea5e28f79ef0b768309f68ddeb4beaf58acb3e84
Kenechukwu Ubesie <49237742+Kenechvkwv@users.noreply.github.com>
2021-03-25 09:14:55 +0100

    script more

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ const quizContainer = document.getElementById("quiz");
 const resultsContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 
-function buildQuiz() {}
+function buildQuiz() {
+  // variable to store the HTML output
+  const output = [];
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for (letter in currentQuestion.answers) {
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+      );
+    }
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+    );
+  });
+
+  // combine the output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+}
 
 function showResults() {}
 
@@ -13,32 +43,32 @@ buildQuiz();
 submitButton.addEventListener("click", showResults);
 
 const myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
     },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
     },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-    }
-  ];
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  }
+];
